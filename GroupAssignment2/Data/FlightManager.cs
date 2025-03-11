bbacd406f1ae4857734e913ecea07b19d22b10e6
Kunj Patel <pkunjk007@gmail.com>
2025-03-11 15:42:59 -0600

    Created the addflights method

--- GroupAssignment2/Data/FlightManager.cs
+++ GroupAssignment2/Data/FlightManager.cs
@@ -8,5 +8,26 @@ namespace GroupAssignment2.Data
 {
     class FlightManager
     {
+        
+        private string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\Resources\Res\flights.csv");
+        public List<Flight> AddFlights()
+        {
+            List<Flight> flights = new List<Flight>();
+            using (StreamReader sw = new StreamReader(filePath))
+            {
+                string line;
+                string[] parts;
+                Flight flight;
+                while (!sw.EndOfStream)
+                {
+                    line = sw.ReadLine();
+                    parts = line.Split(',');
+                    flight = new Flight(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], Convert.ToInt32(parts[6]), Convert.ToDouble(parts[7]));
+                    flights.Add(flight);
+                }
+
+            }
+            return flights;
+
     }
 }
