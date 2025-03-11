788a1bb1503a0a2171434d08db8fe65738d6f522
Kunj Patel <pkunjk007@gmail.com>
2025-03-11 14:49:32 -0600

    Created flight constructor

--- GroupAssignment2/Data/Flight.cs
+++ GroupAssignment2/Data/Flight.cs
@@ -8,5 +8,25 @@ namespace GroupAssignment2.Data
 {
     class Flight
     {
+        public string FlightCode { get; set; }
+        public string FlightName { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Day { get; set; }
+        public string Time { get; set; }
+        public int Seat { get; set; }
+        public  double Price { get; set; }
+
+        public Flight(string flightCode, string flightName, string from, string to, string day, string time, int seat, double price)
+        {
+            FlightCode = flightCode;
+            FlightName = flightName;
+            From = from;
+            To = to;
+            Day = day;
+            Time = time;
+            Seat = seat;
+            Price = price;
+        }
     }
 }
